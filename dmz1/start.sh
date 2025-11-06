5eb5e8953ac12ef7eafa30838bfa104eff8f659b
cperes06 <cperes06@estudiantes.unileon.es>
2025-11-06 15:44:32 +0100

    Modificacion de la practica

--- dmz1/start.sh
+++ dmz1/start.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+# Añadir las rutas del gateway
 route add default gw 10.5.1.1
 route del default gw 10.5.1.254
 
-# Configurar SSH (root:root)
-echo "root:root" | chpasswd
-mkdir -p /var/run/sshd
-
-sed -i 's/^#\?PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed -i 's/^#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-
-# Iniciar servicios
+# Inicialización de servicios
 service apache2 start
 
-runuser -u cowrie -- bash -lc 'cd /home/cowrie/cowrie && source cowrie-env/bin/activate && cowrie start'
+# Mantenemos el COWRIE abierto
+runuser -u cowrie -- bash -lc 'cd /home/cowrie/cowrie && \
+source cowrie-env/bin/activate && \
+cowrie start'
 
-exec /usr/sbin/sshd -D 
+# Configurar SSH, user:passwd
+echo "root:root" | chpasswd
+
+# En caso de no existir, crea /var/run/sshd
+mkdir -p /var/run/sshd
+sed -i 's/^#\?PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/^#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/^#?PubkeyAuthentication\s+.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+
+exec /usr/sbin/sshd -D
